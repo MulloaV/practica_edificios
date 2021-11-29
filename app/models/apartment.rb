00892ef1925489c1329d4cdc5bb4a4c18643bd29
MulloaV <mulloa0179@e-camp.cl>
2021-11-29 20:21:31 -0300

    Cambio menor

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,11 @@
 class Apartment < ApplicationRecord
   belongs_to :building
+
+  validates :status, inclusion: { in: ['arrendado', 'disponible', 'propietario'] }
+
+  STATUS_OPTION = [
+    ['Arrendado','arrendado'],
+    ['Disponible','disponible'],
+    ['Propietario','propietario']]
+    
 end
